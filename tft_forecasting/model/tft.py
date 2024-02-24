6aa8640d68d12e27fed1ed686d35d394405d961d
Thomas Kientz <thomas@kientz.net>
2024-02-24 01:04:56 +0100

    Base TFT class

--- tft_forecasting/model/tft.py
+++ tft_forecasting/model/tft.py
@@ -1,4 +1,7 @@
 import torch.nn as nn
+from torch.nn import LSTM
+from .components.variable_selection import VariableSelection
+from .components.gate_add_norm import GateAddNorm
 
 
 class TemporalFusionTransformer(nn.Module):
@@ -6,8 +9,43 @@ class TemporalFusionTransformer(nn.Module):
 
     def __init__(
         self,
+        mX_encoder,
+        mX_decoder,
+        input_size,
         n_past_inputs,
         n_known_future_inputs,
+        hidden_size,
     ) -> None:
         super().__init__()
-        self.past
+
+        self.encoder_var_selec = VariableSelection(
+            mx=mX_encoder,
+            input_size=input_size,
+            hidden_size=self.hidden_size,
+            dropout=self.dropout,
+        )
+
+        self.decoder_var_selec = VariableSelection(
+            mx=mX_decoder,
+            input_size=input_size,
+            hidden_size=self.hidden_size,
+            dropout=self.dropout,
+        )
+
+        self.lstm_encoder = LSTM(
+            input_size=self.hidden_size,
+            hidden_size=self.hidden_size,
+            num_layers=self.lstm_layers,
+            dropout=self.dropout if self.lstm_layers > 1 else 0,
+            batch_first=True,
+        )
+
+        self.lstm_decoder = LSTM(
+            input_size=self.hidden_size,
+            hidden_size=self.hidden_size,
+            num_layers=self.lstm_layers,
+            dropout=self.dropout if self.lstm_layers > 1 else 0,
+            batch_first=True,
+        )
+
+        self.post_lstm_gan = GateAddNorm(input_size=self.hidden_size, hidden_size=self.hidden_size)
